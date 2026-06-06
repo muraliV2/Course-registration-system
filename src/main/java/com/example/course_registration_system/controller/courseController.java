6d463f6512895553aa63f2268d9583b318eef9a3
Murali <muralisurya2004@gmail.com>
2026-06-06 23:02:31 +0530

    Update courseController.java

--- src/main/java/com/example/course_registration_system/controller/courseController.java
+++ src/main/java/com/example/course_registration_system/controller/courseController.java
@@ -1,5 +1,4 @@
 package com.example.course_registration_system.controller;
-
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
